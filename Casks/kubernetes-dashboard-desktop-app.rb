8380861cfd9413af8b6d63db08e8c40358685319
Yevhen Terentiev <yevhen.terentiev@gmail.com>
2021-04-26 22:14:45 +0300

    v2.2.0 #2

--- Casks/kubernetes-dashboard-desktop-app.rb
+++ Casks/kubernetes-dashboard-desktop-app.rb
@@ -1,6 +1,6 @@
 cask "kubernetes-dashboard-desktop-app" do
   version "2.2.0"
-  sha256 "25174c0267008a506edad6805454d687d9ac5268fe738a08fdd9e5c4dfdbbe03"
+  sha256 "ff777df7c6bb2ce1456e92376c88c649b3301088b144faf237fc20637a22838b"
   url "https://github.com/trntv/kubernetes-dashboard-desktop-app/releases/download/v#{version}/Kubernetes.Dashboard.dmg"
   name "Kubernetes Dashboard"
   desc "Kubernetes Dashboard desktop App (Unofficial)"
